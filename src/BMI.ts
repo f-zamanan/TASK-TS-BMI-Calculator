3630c5eb6a0db94e86d40c55fe4097f85a9714f9
f-zamanan <f.n.zamanan@gmail.com>
2025-04-27 16:44:43 +0300

    haven't finished the last challenge, logic seams correct idk what is the issue

--- src/BMI.ts
+++ src/BMI.ts
@@ -7,7 +7,8 @@
  */
 function calculateBMI(weight: number, height: number): number {
   // Write your code here 👇🏼
-  const BMI = 0;
+  height = height / 100;
+  const BMI = weight / (height * height); // convert to m
   // Stop making changes here!
   return BMI;
 }
@@ -25,7 +26,15 @@ function getBMIState(BMI: number): BMIStatus {
   // Don't change this variable
   let BMIState: BMIStatus = "obese";
   // Write your code here 👇🏼
-
+  if (BMI < 18.5) {
+    BMIState = "underweight";
+  } else if (BMI >= 18.5 && BMI < 25.0) {
+    BMIState = "healthy";
+  } else if (BMI >= 25.0 && BMI < 30.0) {
+    BMIState = "overweight";
+  } else if (BMI >= 30.0) {
+    BMIState = "obese";
+  }
   // Stop making changes here!
   return BMIState;
 }
@@ -44,6 +53,39 @@ function getBMIStateByAge(BMI: number, age: number): HealthStatus {
   // Don't change this variable
   let BMIState: HealthStatus = "not healthy";
   // Write your code here 👇🏼
+
+  if (age >= 19 && age < 25) {
+    // range (19-24, both included)
+    if (BMI >= 19 && BMI < 25) {
+      // range (19-24, both included) // ex: age = 35, bmi = 30 expected: not healthy, it's not running as expected
+      BMIState = "healthy";
+    }
+  } else if (age >= 25 && age < 35) {
+    // range (25-34, both included)
+    if (BMI >= 20 && BMI < 26) {
+      BMIState = "healthy";
+    }
+  } else if (age >= 35 && age < 45) {
+    // range (35-44, both included)
+    if (BMI >= 21 && BMI < 27) {
+      BMIState = "healthy";
+    }
+  } else if (age >= 45 && age < 55) {
+    // range (45-54, both included)
+    if (BMI >= 22 && BMI < 28) {
+      BMIState = "healthy";
+    }
+  } else if (age >= 55 && age < 65) {
+    // range (55-64, both included)
+    if (BMI >= 23 && BMI < 29) {
+      BMIState = "healthy";
+    }
+  } else if (age >= 65 && BMI >= 24) {
+    // range ( equal or ggreater to 65)
+    if (BMI >= 24 && BMI < 30) {
+      BMIState = "healthy";
+    }
+  }
 
   // Stop making changes here!
   return BMIState;
